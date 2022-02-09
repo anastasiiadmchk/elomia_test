1f98072158cd5df0de13abf8e8eb0cb52ae3b60f
anastasiiadmchk <anastasiia.dmchk@gmail.com>
2022-02-09 21:51:27 +0200

    auth screen ui fix

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -8,71 +8,81 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [ElomiaTheme.indigo, Colors.black]),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 450,
-              child: _adImg(),
-            ),
-            Container(
-              color: Colors.black,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [ElomiaTheme.indigo, Colors.black]),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flexible(
+                flex: 2,
+                child: _adImg(context),
               ),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Text(
-                      'Meet Elomia',
-                      style: ElomiaTheme.header,
-                    ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.black,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32.0,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 24.0),
-                    child: Text(
-                      'An AI-powered companion that helps to improve mental health',
-                      style: ElomiaTheme.title,
-                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Text(
+                          'Meet Elomia',
+                          style: ElomiaTheme.header,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 24.0),
+                        child: Text(
+                          'An AI-powered companion that helps to improve mental health',
+                          style: ElomiaTheme.title,
+                        ),
+                      ),
+                      SignInWithAppleButton(
+                        onPressed: () async {
+                          // final authData =
+                          //     await BlocProvider.instance.authBloc.getAuthData();
+                          Navigator.of(context)
+                              .pushReplacementNamed(Routes.concernsScreen);
+                        },
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12.0)),
+                        style: SignInWithAppleButtonStyle.white,
+                        text: 'Continue with Apple',
+                        height: 48,
+                      )
+                    ],
                   ),
-                  SignInWithAppleButton(
-                    onPressed: () async {
-                      // final authData =
-                      //     await BlocProvider.instance.authBloc.getAuthData();
-                      Navigator.of(context)
-                          .pushReplacementNamed(Routes.concernsScreen);
-                    },
-                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                    style: SignInWithAppleButtonStyle.white,
-                    text: 'Continue with Apple',
-                    height: 48,
-                  )
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-  _adImg() => Stack(
+  _adImg(BuildContext context) => Stack(
         alignment: AlignmentDirectional.topCenter,
         children: [
           Positioned(
-              top: -50,
+              top: -(1 / MediaQuery.of(context).size.aspectRatio) * 15,
               child: Image.asset(
                 'assets/elomia/elomia.png',
-                height: 522,
+                  height: MediaQuery.of(context).size.height *
+                      1.35 *
+                      MediaQuery.of(context).size.aspectRatio
               )),
           Align(
             alignment: Alignment.bottomCenter,
